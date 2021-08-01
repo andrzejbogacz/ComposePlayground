66980ee595885137027146c48ba51694238642fb
Andrzej Bogacz <ashaneen69@gmail.com>
2021-08-01 17:03:51 +0200

    1

--- app/src/main/java/com/slayingthedragon/showcase/MainActivity.kt
+++ app/src/main/java/com/slayingthedragon/showcase/MainActivity.kt
@@ -29,23 +29,6 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BookroomShowcaseTheme {
-        Greeting("Android")
-    }
-}@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BookroomShowcaseTheme {
-        Greeting("Android")
-    }
 }@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
