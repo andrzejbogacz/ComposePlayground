7ab45d67771d81028f76cb16c4ab6420ebc2bf07
Andrzej Bogacz <ashaneen69@gmail.com>
2021-10-20 17:32:26 +0200

    gradle changes to make it compatible with new non alpha gradle fox android studio version

--- feature_search/build.gradle.kts
+++ feature_search/build.gradle.kts
@@ -10,14 +10,14 @@ android{
         testInstrumentationRunner ="androidx.test.runner.AndroidJUnitRunner"
     }
     lint {
-        abortOnError = true
-        checkAllWarnings = true
-        warningsAsErrors = true
+        isAbortOnError = true
+        isCheckAllWarnings = true
+        isWarningsAsErrors = true
     }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
