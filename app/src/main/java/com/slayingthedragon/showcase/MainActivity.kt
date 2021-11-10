1e07749458c3c1c984dd383c7cf40c86f30a4fcc
Andrzej Bogacz <ashaneen69@gmail.com>
2021-11-10 18:02:30 +0100

    deleting left over files after rebranding

--- app/src/main/java/com/slayingthedragon/showcase/MainActivity.kt
+++ app/src/main/java/com/slayingthedragon/showcase/MainActivity.kt
@@ -3,13 +3,10 @@ package com.slayingthedragon.showcase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
         }
     }
